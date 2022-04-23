89cd574e3db0379faf9a31b94125f54ac0412641
Allyson Eduardo dos Santos Silva <allysoneduardo04@gmail.com>
2022-04-23 12:13:09 -0300

    feat: add option 'trash' and 'dont rename file' based on input

--- fileRenamer.ps1
+++ fileRenamer.ps1
@@ -1,12 +1,37 @@
 #folder files setup
 $files=gci -File -Name;
- 
+$trashcode=1
+
 foreach($file in $files){
     $OriginalFilename = $file; 
-    Invoke-Item $pwd\$OriginalFilename; 
+    
+    # opens default program defined to file
+    explorer $pwd\$OriginalFilename
+    # $wprocess = (start-process explorer $pwd\$OriginalFilename -passthru)
+
+    # Get filename without extension
     $OriginalFilenameWithoutExtension = Split-Path $OriginalFilename -LeafBase;
+
+    # get name input
     $newFilenameWithoutExtension = Read-Host -Prompt 'New Filename?';
-    $newFilename = $OriginalFilename.replace($OriginalFilenameWithoutExtension, $newFilenameWithoutExtension);
-    Rename-Item -Path $pwd\$OriginalFilename -NewName $newFileName -Confirm;
-    Start-Sleep -s 2
+
+
+    switch ($newFilenameWithoutExtension)
+    {
+        # If nothing is input, don't change the filename
+	    "" {$newFilename = $OriginalFilename; break;}
+
+        # if input is "trash", change the file so it can be deleted after all files renaming
+	    "trash" {$trashcode++; $newFilename = $OriginalFilename.replace($OriginalFilenameWithoutExtension, ("trash"+$trashcode)); break;}
+
+        # if any other string except  "" and "trash" is input, rename with it
+	    Default {$newFilename = $OriginalFilename.replace($OriginalFilenameWithoutExtension, $newFilenameWithoutExtension); break;}
+    }
+    
+    echo $newFileName;
+
+    #echo $wprocess;
+    #taskkill /PID $wprocess.id /f;
+	
+    Rename-Item -Path $pwd\$OriginalFilename -NewName $newFileName;
 }
--- readme.txt
+++ readme.txt
@@ -1,12 +1,16 @@
 A minor script to learn a few functions with Powershell. 
 It renames any file (not folders) from where it is run, only changing the filename, so the extension is preserved.
 
-Steps of:
+Steps:
 1. Opens the file, so you can know the content inside it;
-2. Asks the new filename for it;
-3. Asks confirmation for the renaming;
+2. Asks the new filename for it, depending on the input, it can make other things such as:
+- Don't input anything => don't change the filename;
+- Input "trash" => File is renamed to "trash{number}" with {number} being a increasing number. Used so after renaming all files, it can be detected which files are for deleting;
+- Anything else => Change the filename with this input.
+3. Prints new filename;
 4. Repeats steps 1 to 3 until there's no other file remaining.
 
-
+To-do
+- Get process Id and close it after input.
 
 Made on Powershell 7.x
